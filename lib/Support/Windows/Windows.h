cdefcbbc52155dee572242574671e8266e05bd35
NAKAMURA Takumi <geek4civic@gmail.com>
2010-12-07 10:33:35 +0900

    Windows/Windows.h: mingw64 might have _WIN32_WINNT.

--- lib/Support/Windows/Windows.h
+++ lib/Support/Windows/Windows.h
@@ -17,9 +17,15 @@
 //===----------------------------------------------------------------------===//
 
 // Require at least Windows 2000 API.
+#ifdef _WIN32_WINNT
+#if _WIN32_WINNT < 0x0500
+#error "We assume at least Windows 2000 API."
+#endif
+#else
 #define _WIN32_WINNT 0x0500
 #define _WIN32_IE    0x0500 // MinGW at it again.
 #define WIN32_LEAN_AND_MEAN
+#endif
 
 #include "llvm/Config/config.h" // Get build system configuration settings
 #include <Windows.h>
